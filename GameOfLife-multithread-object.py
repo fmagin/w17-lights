b07fb62780fc4e56bf755059a79abbf7b9382d9f
Komasa <komasa@tfwno.gf>
2017-04-21 22:41:39 +0200

    Make OOP version work

--- GameOfLife-multithread-object.py
+++ GameOfLife-multithread-object.py
@@ -2,6 +2,7 @@
 from lights import Client, ColorSpace
 import random
 import time
+import math
 
 
 class GameOfLife():
@@ -10,44 +11,44 @@ class GameOfLife():
     RULE90 = {0b111: 0, 0b110: 1, 0b101: 0, 0b100: 1, 0b011: 1, 0b010: 0, 0b001: 1, 0b000: 0}
     RULE110 = {0b111: 0, 0b110: 1, 0b101: 1, 0b100: 0, 0b011: 1, 0b010: 1, 0b001: 1, 0b000: 0}
     RULE184 = {0b111: 1, 0b110: 0, 0b101: 1, 0b100: 1, 0b011: 1, 0b010: 0, 0b001: 0, 0b000: 0}
-
-    @staticmethod
-    def get_rand_state(length, chance):
-        """ Generates random starting state"""
-        return [1 if random.random() < chance else 0 for _ in range(length)]
-
-    def __init__(self, ledcount):
+    hue, saturation = 0.1, 0.8
+    LEDCOUNT = 1600
+    state = [] # type: List[(float, float, float)]
+    def __init__(self):
+        """Initialize with a random start state"""
         self.connection = Client("light.w17.io", 1337)
-        self.state = self.get_rand_state(ledcount, 0.2)
+        self.state = [(self.hue, self.saturation, 1.0) if random.random() < 0.5
+                      else (self.hue, self.saturation, 0.0) for _ in range(self.LEDCOUNT)]
 
     def push(self):
         """Pushes the current state to the remote server"""
         self.connection.push(self.state, ColorSpace.HSV)
 
 
-
     def tick(self, rule, ticklength):
         """ Continues the game one tick according to a set of rules """
         time.sleep(ticklength)
+        hue = self.hue
+        saturation = self.saturation
         new_state = []
         for a in range(len(self.state)):
             try:
                 l_parent, top_parent, r_parent = self.state[a-1], self.state[a], self.state[a+1]
             except IndexError:
                 l_parent, top_parent, r_parent = self.state[a-1], self.state[a], self.state[0]
-            parents = (l_parent <<2) + (top_parent << 1) + r_parent
-            new_state.append(rule[parents])
+            parent_value = (int(math.ceil(l_parent[2])) <<2) + (int(math.ceil(top_parent[2])) << 1) + int(math.ceil(r_parent[2]))
+            value = rule[parent_value]
+            new_state.append((hue, saturation, value))
         self.state = new_state
-
-    
+        self.hue = (self.hue + 0.1) % 360
 
 if __name__ == "__main__":
-    animation = GameOfLife(1600)
+    animation = GameOfLife()
     while True:
         animation.tick(GameOfLife.RULE30, 0.2)
         animation.push()
 
-
+"""
 H,S,V = 0.1, 0.8, 0.8 
 while True:
     time.sleep(0.2)
@@ -59,4 +60,4 @@ while True:
     print("Value is: ", )    
     data = map(lambda cell:(H,S,V) if cell==1 else (0,0,0), self.state)
     client.push(data, ColorSpace.HSV)
-
+"""
